25bd1bce6eda4ae6559becf9e8e29cf86470f981
Charlotte Tennant <charlottetennant@Charlotte-Ts-MacBook-Air-2.local>
2023-04-19 08:06:44 +0100

    added serverless

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
-const messageNode = document.getElementById('message');
+const messageNode = document.getElementById("message");
+
+const render = (message = "", DOMNode = messageNode) => {
+  DOMNode.textContent = message;
+};
+
+const getData = async () => {
+  try {
+    const response = await fetch("/.netlify/functions/hello-world");
+    if (!response.ok) throw response;
+    const data = await response.json();
+    console.log(data);
+    render(data.message);
+  } catch (err) {
+    alert("Error fetching. Check the console.");
+    console.log(err);
+  }
+};
+
+getData();
